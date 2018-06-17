113d9738c9335fed6c1606d915add3beb7ac9e09
Thomas Alexnat <thomas@alexnat.de>
2018-06-17 12:29:57 +0200

    added clock display

--- newfoto/TabBarController.swift
+++ newfoto/TabBarController.swift
@@ -12,6 +12,42 @@ class TabBarController: UITabBarController {
 
     // will be true when the focus in the
     // first subcontroller was loaded (the focus environment)
+    let font = UIFont.systemFont(ofSize: 29, weight: .bold)
+    let tabFont = UIFont.systemFont(ofSize: 40, weight: .bold)
+    var label = UILabel(frame: CGRect(x: 1795, y: 42, width: 150, height: 60))
+    
+    
+    func getLabelColor() -> UIColor {
+        let defaultLabelColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.8)
+        
+        guard(self.traitCollection.responds(to: #selector(getter: UITraitCollection.userInterfaceStyle)))
+            else { return  defaultLabelColor }
+        
+        let style = self.traitCollection.userInterfaceStyle
+        
+        switch style {
+        case .light:
+            return defaultLabelColor
+        case .dark:
+            return UIColor(red: 1, green: 1, blue: 1, alpha: 0.85)
+            
+        case .unspecified:
+            return defaultLabelColor
+        }
+    }
+    
+    
+    @objc func updateClock() -> Void {
+        
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.setLocalizedDateFormatFromTemplate("HH:mm")
+        let time = dateFormatter.string(from: date)
+        
+        
+        label.text =  time
+    }
+    
     
 
     var isAlbumLoaded = false
@@ -19,20 +55,47 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        setupUI()
+        
+        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateClock), userInfo: nil, repeats:true);
+        
+        
+        
+    }
+    
+    
+    
+    
+    func setupUI(){
+        
+        
+        
+        
+        label.font = font
+        label.textColor = getLabelColor()
+        
+        
+        //  label.attributedText(attributes, for: .normal)
+        
+        
+        tabBar.addSubview(label)
+        label.numberOfLines = 0
+        tabBar.bringSubview(toFront: label)
+        
+        
+        
+        // style the tabs
+        // provide a better accessibility styling for the tabbar
         let attributes: [NSAttributedStringKey : Any] =
             [
-                .font: UIFont.systemFont(ofSize: 40, weight: .bold)
+                .font: tabFont
         ]
         
-       
         if let tabs = tabBar.items {
             for tab in tabs{
                 tab.setTitleTextAttributes(attributes, for: .normal)
                 tab.setTitleTextAttributes(attributes, for: .selected)
-                
-                
-                //tab.setTitleTextAttributes([NSAttributedStringKey.font : UIFont.systemFont(ofSize: 40, weight: .bold)], for: .selected)
-                
             }
         }
     }
